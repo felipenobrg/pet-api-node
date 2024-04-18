44b5708e13d2e49c37d1809f268ec4d9b176334b
Felipe Nóbrega de Almeida <felipenobrega2012@gmail.com>
2024-04-18 20:01:10 -0300

    feat: adding db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-import express, { Response } from "express";
+import express from "express";
 import router from "./routes";
+import "reflect-metadata"
+import { AppDataSource } from "./config/dataSource";
 
 const app = express();
 app.use(express.json());
 router(app);
+
+AppDataSource.initialize().then(() => {
+    console.log("DB conected")
+}).catch((error) => {
+    console.log(error)
+})
 
 export default app;
